7e08b441d4eb737b4e34ab3ccf1f12e079f83ca5
Ito Yoshiaki <ito-yoshiaki@skw.canon-soft.co.jp>
2016-12-06 17:28:50 +0900

    multer-azure

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-var express = require('express');
-var multer  = require('multer');
-var upload = multer({ dest: 'uploads/' });
-var app = express();
-
-app.post('/', upload.single('data'), function (req, res) {
-  console.log(req.file);
-  res.end('success');
-});
-
-app.listen(4000, function () {
-  console.log('Example app listening on port 4000!');
-});
+var express = require('express')
+var multer = require('multer')
+var multerAzure = require('multer-azure')
+ 
+var app = express()
+ 
+var upload = multer({ 
+  storage: multerAzure({
+    account: 'linecars', //The name of the Azure storage account 
+    key: 'F5tVm8iweO5rPfasM/QMisLu9feX3MPWTzt8YhdAHc2TdedWRWfl4bge7IQy2N9XtFinZJhFSX6/fNePYlSJoA==', //A key listed under Access keys in the storage account pane 
+    container: 'linecars'  //Any container name, it will be created if it doesn't exist 
+  })
+})
+ 
+app.post('/', upload.any(), function (req, res, next) {
+  console.log(req.files)
+  res.status(200).send('Uploaded: ' + req.files)
+})
