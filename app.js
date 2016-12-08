28155e0cc1175ddeb01b409b0a23ed26044db80b
Ito Yoshiaki <ito-yoshiaki@skw.canon-soft.co.jp>
2016-12-08 16:47:57 +0900

    テスト

--- app.js
+++ app.js
@@ -1,18 +1,47 @@
-var express = require('express')
-var multer = require('multer')
-var multerAzure = require('multer-azure')
- 
-var app = express()
- 
-var upload = multer({ 
-  storage: multerAzure({
-    account: 'linecars', //The name of the Azure storage account 
-    key: 'F5tVm8iweO5rPfasM/QMisLu9feX3MPWTzt8YhdAHc2TdedWRWfl4bge7IQy2N9XtFinZJhFSX6/fNePYlSJoA==', //A key listed under Access keys in the storage account pane 
-    container: 'linecars'  //Any container name, it will be created if it doesn't exist 
-  })
+// curl "localhost:3000/img" -X POST -F data=@test.jpg
+// http://localhost:3000/img/xxxxx
+
+var TMPDIR = 'c:\\temp\\'
+var express = require('express');
+var ejs = require("ejs");
+var multer  = require('multer');
+var upload = multer({ dest: TMPDIR });
+var fs = require('fs');
+
+var app = express();
+app.engine('ejs', ejs.renderFile);
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', function(req, res){
+    res.render('test.ejs', 
+        {title: 'Test Page' , 
+            content: 'this is test.'});
 })
- 
-app.post('/', upload.any(), function (req, res, next) {
-  console.log(req.files)
-  res.status(200).send('Uploaded: ' + req.files)
+app.get('/input', function(req, res){
+    res.render('input.ejs');
+})
+app.post('/msg', function(req, res){
+    console.log(req.body);
+    res.render('test.ejs', 
+        {title: 'Message Send' , 
+            content: req.body.msg});
+})
+
+
+app.get('/img/:fname', function(req, res){
+    console.log(req.params.fname);
+    fs.readFile(TMPDIR + req.params.fname, function(err, data){
+        res.set('Content-Type', 'image/jpeg');
+        res.send(data);
+    });
+});
+
+app.post('/img', upload.single('data'), function (req, res) {
+    console.log(req.file);
+    res.end('success');
+});
+
+var server = app.listen(3000, function(){
+    console.log('Server is running!');
 })
