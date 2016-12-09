fcce5b480de384e84e3c095ac92ce765adfb1ccf
Ito Yoshiaki <ito-yoshiaki@skw.canon-soft.co.jp>
2016-12-09 10:03:18 +0900

    4

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const express = require('express');
 const app = express();
 var ejs = require("ejs");
 
+app.engine('ejs', ejs.renderFile);
 
-app.get('/', function(req, res) {
-  res.send('Hello World3')
-});
- 
+
+//app.get('/', function(req, res) {
+//  res.send('Hello World3')
+//});
+app.get('/', function(req, res){
+    res.render('test.ejs', 
+        {title: 'Test Page4' , 
+            content: 'this is test.'});
+})
+
 //app.listen(PORT);
 var server = app.listen(PORT, function(){
     console.log('Server is running!');
