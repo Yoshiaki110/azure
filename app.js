f8d0a5c2341c3981f2f8126345b29629eb0be9aa
Ito Yoshiaki <ito-yoshiaki@skw.canon-soft.co.jp>
2016-12-09 10:00:42 +0900

    3

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 //app.js
+const PORT = process.env.PORT || 3000;
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
- 
+var ejs = require("ejs");
+
+
 app.get('/', function(req, res) {
-  res.send('Hello World2')
+  res.send('Hello World3')
 });
  
 //app.listen(PORT);
