1edae8ec5aaeb3806ca7a35207fa3c256167aadd
Ito Yoshiaki <ito-yoshiaki@skw.canon-soft.co.jp>
2016-12-09 10:23:29 +0900

    12

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 const ejs = require("ejs");
 const multer  = require('multer');
 const upload = multer({ dest: TMPDIR });
-//const fs = require('fs');
+const fs = require('fs');
 
 app.engine('ejs', ejs.renderFile);
 const bodyParser = require('body-parser')
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/', function(req, res){
     res.render('test.ejs', 
         {title: 'Test Page' , 
-            content: 'this is test.11'});
+            content: 'this is test.12'});
 })
 app.get('/input', function(req, res){
     res.render('input.ejs');
@@ -28,6 +28,19 @@ app.post('/msg', function(req, res){
         {title: 'Message Send' , 
             content: req.body.msg});
 })
+
+app.get('/img/:fname', function(req, res){
+    console.log(req.params.fname);
+    fs.readFile(TMPDIR + req.params.fname, function(err, data){
+        res.set('Content-Type', 'image/jpeg');
+        res.send(data);
+    });
+});
+
+app.post('/img', upload.single('data'), function (req, res) {
+    console.log(req.file);
+    res.end('success');
+});
 
 var server = app.listen(PORT, function(){
     console.log('Server is running!');
