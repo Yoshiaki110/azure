cca9b1b483e884070087fe0ecb19c20bf457cf35
Ito Yoshiaki <ito-yoshiaki@skw.canon-soft.co.jp>
2016-12-09 10:09:10 +0900

    7

--- app.js
+++ app.js
@@ -2,20 +2,16 @@
 const PORT = process.env.PORT || 3000;
 const express = require('express');
 const app = express();
-var ejs = require("ejs");
+const ejs = require("ejs");
 
 app.engine('ejs', ejs.renderFile);
-var bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
-//app.get('/', function(req, res) {
-//  res.send('Hello World3')
-//});
 app.get('/', function(req, res){
     res.render('test.ejs', 
         {title: 'Test Page' , 
-            content: 'this is test.6'});
+            content: 'this is test.7'});
 })
 app.get('/input', function(req, res){
     res.render('input.ejs');
@@ -27,7 +23,6 @@ app.post('/msg', function(req, res){
             content: req.body.msg});
 })
 
-//app.listen(PORT);
 var server = app.listen(PORT, function(){
     console.log('Server is running!');
 })
